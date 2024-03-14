c27f7b9b4adf36c79ca01a8de262088c30eeb592
Yury Stolarz Santana <ysantana@telluscompany.com.br>
2024-03-14 20:05:06 -0300

    feat: add property id

--- src/domain/entity/vehicle.entity.ts
+++ src/domain/entity/vehicle.entity.ts
@@ -1,10 +1,21 @@
+import { randomUUID } from 'crypto';
 import { IVehicle } from '../interface/vehicle.interface';
 
 export class Vehicle {
   private _props: IVehicle;
+  private _id: string;
 
-  constructor(props: IVehicle) {
+  constructor(props: IVehicle, id?: string) {
     this._props = { ...props };
+    this._id = id ?? randomUUID();
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
   }
 
   get plate() {
