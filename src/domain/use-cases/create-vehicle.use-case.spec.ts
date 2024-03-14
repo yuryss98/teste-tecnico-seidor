93fa2a95e341184e54db7f2f336f859de99d5795
Yury Stolarz Santana <ysantana@telluscompany.com.br>
2024-03-14 20:06:01 -0300

    test: create vehicle use case

---
+++ src/domain/use-cases/create-vehicle.use-case.spec.ts
@@ -0,0 +1,20 @@
+import InMemoryVehicleRepository from '../../db/in-memory-vehicle-repository';
+import CreateVehicleUseCase from './create-vehicle.use-case';
+
+const newVehicle = {
+  plate: 'ABC-1234',
+  color: 'Black',
+  brand: 'Porsche',
+};
+
+describe('Vehicle use case', () => {
+  it('Should be able to create a new vehicle', async () => {
+    const vehicleRepository = new InMemoryVehicleRepository();
+    const createVehicle = new CreateVehicleUseCase(vehicleRepository);
+
+    const vehicle = await createVehicle.execute(newVehicle);
+
+    expect(vehicleRepository.vehicles).toHaveLength(1);
+    expect(vehicleRepository.vehicles[0]).toEqual(vehicle);
+  });
+});
