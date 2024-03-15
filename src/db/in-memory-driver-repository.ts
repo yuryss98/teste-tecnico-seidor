50dd61cafdafe40f22cfb1c990bd207dd2297d50
Yury Stolarz Santana <ysantana@telluscompany.com.br>
2024-03-14 22:44:12 -0300

    feat: create in memory driver repository

---
+++ src/db/in-memory-driver-repository.ts
@@ -0,0 +1,47 @@
+import { Driver } from '../domain/entity/driver.entity';
+import DriverRepository from '../domain/repository/driver.repository';
+
+export default class InMemoryDriverRepository implements DriverRepository {
+  public drivers: Driver[] = [];
+
+  async create(driver: Driver): Promise<string> {
+    this.drivers.push(driver);
+    return driver.id;
+  }
+
+  async findById(driverId: string): Promise<Driver | null> {
+    const driver = this.drivers.find(({ id, status }) => id === driverId && status);
+    if (!driver) return null;
+    return driver;
+  }
+
+  async findAll(name?: string): Promise<Driver[]> {
+    if (name) {
+      return this.drivers.filter((driver) => (
+        driver.name.toLowerCase() === name.toLowerCase()
+      ));
+    }
+
+    return this.drivers;
+  }
+
+  async update(newDriver: Driver): Promise<void> {
+    this.drivers.map((driver) => {
+      if (driver.id === newDriver.id) {
+        return newDriver;
+      }
+
+      return driver;
+    });
+  }
+
+  async delete(deletedDriver: Driver): Promise<void> {
+    this.drivers.map((driver) => {
+      if (driver.id === deletedDriver.id) {
+        return deletedDriver;
+      }
+
+      return driver;
+    });
+  }
+}
