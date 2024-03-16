1edb805ec564768854a9150b0788b58319dfaa74
Yury Stolarz Santana <ysantana@telluscompany.com.br>
2024-03-16 13:45:07 -0300

    fix: add the id, startDate and endDate properties to the vehicle utilization entity

--- src/domain/entity/vehicle-utilization.entity.ts
+++ src/domain/entity/vehicle-utilization.entity.ts
@@ -1,10 +1,26 @@
-import { IVehicleUtilization } from '../interface/vehicle.interface';
+import { randomUUID } from 'crypto';
+import { IVehicleUtilization } from '../interface/vehicle-utilization.interface';
 
-export class VehicleUtilization {
+export class VehicleUtilizationEntity {
   private _props: IVehicleUtilization;
+  private _id: string;
+  private _startDate: Date;
+  private _endDate: Date;
+  private _vehicleInUse: boolean;
 
-  constructor(props: IVehicleUtilization) {
+  constructor(props: IVehicleUtilization, id?: string, startDate?: Date) {
     this._props = { ...props };
+    this._id = id ?? randomUUID();
+    this._startDate = startDate ?? new Date();
+    this._vehicleInUse = true;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
   }
 
   get driver() {
@@ -16,14 +32,23 @@ export class VehicleUtilization {
   }
 
   get startDate() {
-    return this._props.startDate;
+    return this._startDate;
   }
 
   get endDate() {
-    return this._props.endDate;
+    return this._endDate;
   }
 
   get utilizationMotive() {
     return this._props.utilizationMotive;
+  }
+
+  get vehicleInUse() {
+    return this._vehicleInUse;
+  }
+
+  finishVehicleUtilization() {
+    this._endDate = new Date();
+    this._vehicleInUse = false;
   }
 }
