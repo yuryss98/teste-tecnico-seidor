a381d4ff7b53b15a051a2cf179225cb64dbc4d06
Yury Stolarz Santana <ysantana@telluscompany.com.br>
2024-03-17 16:44:04 -0300

    chore: install and add morgan to log

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import driverRoutes from '../routes/driver.routes';
 import errorHandler from '../middleware/error-handle';
 
 const app = express();
+app.use(morgan('combined'));
 app.use(helmet());
 app.use(express.json());
 
